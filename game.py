6f56d28c08e84d7b4171ab8eddb7424db6f3057f
Steve <stevebedard29@gmail.com>
2023-07-18 09:41:07 -0400

    added game.py

---
+++ game.py
@@ -0,0 +1,16 @@
+class Game:
+    bank_of_words = ["Elephant", "Morning", "Electronic", ]
+
+    def __init__(self):
+        self.word_to_guess = ""
+        self.number_of_tries = 5
+        self.is_game_over = False
+        self.guessed_letters = []
+
+    def guess_letter(self, letter):
+        pass
+
+    def guess_word(self, word):
+        pass
+
+    
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from player import Player
+from game import Game
+
 print("Hi, and welcome to hangman !")
+
+player_name = input("What is your name ?")
+
+player_1 = Player(player_name)
+
