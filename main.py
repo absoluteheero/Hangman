af3dddb223de69207b95c045a1a0511b6e0150e2
Steve <stevebedard29@gmail.com>
2023-07-20 10:08:34 -0400

    added logo image

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from player import Player
 from game import Game
 
 print("\n")
-print("Hi, and welcome to hangman !")
 
 player_name = input("What is your name ?")
 
 print("\n")
+
 
 player_1 = Player(player_name)
 
